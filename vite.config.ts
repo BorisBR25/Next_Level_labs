5bf053457b2cd2aecf8872a6fd85c7b82fc17921
BorisBR25 <jbarriosricardo@gmail.com>
2024-06-20 09:10:53 -0500

    uninstall gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/deploy-github/",
   plugins: [react()],
   resolve: {
     alias : [{ find: '@', replacement: path.resolve(__dirname, 'src')}]
