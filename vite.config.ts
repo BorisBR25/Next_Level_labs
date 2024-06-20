59b88a77db74f261f4bd968a086a67dff6a24ce0
BorisBR25 <jbarriosricardo@gmail.com>
2024-06-20 08:50:08 -0500

    homepage modified

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploy-github/",
+  //base: "/deploy-github/",
   plugins: [react()],
   resolve: {
     alias : [{ find: '@', replacement: path.resolve(__dirname, 'src')}]
